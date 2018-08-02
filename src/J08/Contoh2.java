a7d928cf3546f1b5d780fbd12244d9e15d7249c7
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-08-02 09:09:33 +0700

    bil 1-n

--- src/J08/Contoh2.java
+++ src/J08/Contoh2.java
@@ -4,24 +4,28 @@
  * and open the template in the editor.
  */
 package J08;
-
+import java.util.Scanner;
 /**
  *
  * @author WINDOWS 10
  */
 public class Contoh2 {
     public static void main(String[] args) {
-        
+        Scanner baca = new Scanner(System.in);
+       
         //For
+        System.out.print("Masukkan nilai n atau nilai akhir: ");
         int x;
-        for(x = 1; x <= 10; x++)
+        int n;
+        n = baca.nextInt();
+        for(x = 1; x <= n; x++)
         {
             System.out.print(x + " ");
         }
         
         //WHILE
         x = 1;
-        while(x <= 10)
+        while(x <= n)
         {
             System.out.println(x + " ");
             x++;
@@ -31,10 +35,10 @@ public class Contoh2 {
         x = 1;
         do
         {
-            System.out.println(x + " ");
+            System.out.print(x + " ");
             x++;
         }
-        while(x <= 10);
+        while(x <= n);
 
     }
 }
