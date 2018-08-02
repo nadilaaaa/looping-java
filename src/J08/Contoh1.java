8391ab4e7ed79d9c5fc4f301b72edfd08f9673f0
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-08-02 07:48:17 +0700

    For While Do-While

---
+++ src/J08/Contoh1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J08;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Contoh1 {
+    public static void main(String[] args) {
+        
+        //pengulangan for
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK TELKOM MALANG");
+        }
+        //pengulangan while
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK TELKOM MALANG");
+            i++;
+        }
+        //pengulangan DO WHILE
+        int j = 1;
+        do 
+            
+        {
+            System.out.println("SMK TELKOM MALANG");
+            j++;
+        }
+        while(j <= 5);
+    }
+}
